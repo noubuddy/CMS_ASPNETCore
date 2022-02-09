a108b7210e66992ec84ef13e5a62b2dd6f6fceb7
noubuddy <anton.nitsiportsik2@tptlive.ee>
2022-02-09 11:13:32 +0200

    Add project files.

---
+++ CMS_ASPNETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CMS_ASPNETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
